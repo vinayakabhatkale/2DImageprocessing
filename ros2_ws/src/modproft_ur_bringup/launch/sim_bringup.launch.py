c8f3b8ba68c1e9c969f7bec50061ea87859a358e
Sergej Krasnikov <sergej.krasnikov@uni-a.de>
2022-11-08 14:09:03 +0100

    local description

--- ros2_ws/src/modproft_ur_bringup/launch/sim_bringup.launch.py
+++ ros2_ws/src/modproft_ur_bringup/launch/sim_bringup.launch.py
@@ -15,10 +15,6 @@ def generate_launch_description():
 
     moveit_config = os.path.join(
         get_package_share_directory('modproft_ur_bringup'), 'launch', 'ur_moveit.launch.py')
-    
-    octomap = os.path.join(
-        get_package_share_directory('octomap_server2'), 'launch', 'octomap_server_launch.py')
-
 
     ur_bringup = IncludeLaunchDescription(PythonLaunchDescriptionSource(ur_bringup),
                     launch_arguments={"ur_type": "ur5e",
@@ -34,22 +30,7 @@ def generate_launch_description():
                                       "launch_rviz":"true",
                                      }.items())
 
-    
-    octomap = IncludeLaunchDescription(PythonLaunchDescriptionSource(octomap))
-
-    octomap_tf = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        name="octomap_tf",
-        output="log",
-        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "custom_camera_link", "world"],
-        parameters=[{'use_sim_time': 'True'}]
-    )
-
     ld.add_action(ur_bringup)
     ld.add_action(moveit_config)
-    ld.add_action(octomap_tf)
-    ld.add_action(octomap)
-
 
     return ld
