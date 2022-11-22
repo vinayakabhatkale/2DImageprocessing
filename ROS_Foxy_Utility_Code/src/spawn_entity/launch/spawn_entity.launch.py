2693049c801711445a4f35df0139c0b42e83c201
Max Schnitzler <maximilian.schnitzler@hs-augsburg.de>
2022-11-22 12:00:39 +0100

    Docker Rebuild, Adapter and PCB integration

---
+++ ROS_Foxy_Utility_Code/src/spawn_entity/launch/spawn_entity.launch.py
@@ -0,0 +1,189 @@
+import xml.etree.ElementTree as ET
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, RegisterEventHandler
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+
+
+def launch_setup(context, *args, **kwargs):
+    package_name = LaunchConfiguration('package_name')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    entity_type = LaunchConfiguration('entity_type').perform(context)
+    entity_name = LaunchConfiguration('entity_name').perform(context)
+    urdf_xacro_name = LaunchConfiguration('urdf_xacro_name')
+    x_pos = float(LaunchConfiguration('x', default='0').perform(context))
+    y_pos = float(LaunchConfiguration('y', default='0').perform(context))
+    z_pos = float(LaunchConfiguration('z', default='0').perform(context))
+    r_rot = LaunchConfiguration('R', default='0').perform(context)
+    p_rot = LaunchConfiguration('P', default='0').perform(context)
+    y_rot = LaunchConfiguration('Y', default='0').perform(context)
+    urdf_args = LaunchConfiguration('urdf_args').perform(context)
+
+    # Load controller configurations
+    entity_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(package_name), "config", "{}_controller.yaml".format(entity_type)
+        ]
+    )
+
+    # Load entity description
+    if not urdf_args:
+        entity_description = Command(
+            [
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution([FindPackageShare(package_name), 'urdf', urdf_xacro_name])
+                ,
+                " ",
+                "package_name:=",
+                package_name,
+                " ",
+                "entity_type:=",
+                entity_type,
+                " ",
+                "entity_name:=",
+                entity_name
+            ]
+        )
+    else:
+        entity_description = Command(
+            [
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution([FindPackageShare(package_name), 'urdf', urdf_xacro_name])
+                ,
+                " ",
+                "package_name:=",
+                package_name,
+                " ",
+                "entity_type:=",
+                entity_type,
+                " ",
+                "entity_name:=",
+                entity_name,
+                " ",
+                "args:=",
+                urdf_args
+            ]
+        )
+
+    # Necessary nodes and delays
+    spawn_within_gazebo = Node(
+        package="spawn_entity",
+        executable="spawn_entity_node.py",
+        output="both",
+        arguments=['-description', entity_description,
+                   '-name', entity_name,
+                   '-namespace', entity_name,
+                   '-x', str(x_pos / 1e3),
+                   '-y', str(y_pos / 1e3),
+                   '-z', str(z_pos / 1e3),
+                   '-R', r_rot,
+                   '-P', p_rot,
+                   '-Y', y_rot]
+    )
+    spawn_controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': entity_description},
+                    entity_controllers],
+        arguments=['-namespace', entity_name],
+        output="both"
+    )
+    delay_spawn_controller_manager = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_within_gazebo,
+            on_exit=[spawn_controller_manager]
+        )
+    )
+
+    spawn_jsb = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=['joint_state_broadcaster',
+                   '--controller-manager',
+                   '{}/controller_manager'.format(entity_name)]
+    )
+    spawn_rsp = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[{'use_sim_time': use_sim_time,
+                     'robot_description': entity_description}],
+        # 'tf_prefix': "{}_{}".format(pcb_type, i)}],
+        # arguments=['-namespace', "{}_{}".format(pcb_type, i)]
+    )
+
+    return [spawn_within_gazebo]#, delay_spawn_controller_manager]#, spawn_jsb]#, spawn_rsp]
+
+
+def generate_launch_description():
+    declared_arguments = [
+        DeclareLaunchArgument(
+            "package_name",
+            description="Name of the package from whom the entity is to be spawned."
+        ),
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value='true',
+            description="Use sim time of Gazebo simulation environment.",
+        ),
+        DeclareLaunchArgument(
+            "entity_type",
+            description="The type of the entity."
+                        "This has to correspond to file names without the respective endings!",
+        ),
+        DeclareLaunchArgument(
+            "entity_name",
+            description="The name and namespace of the entity.",
+        ),
+        DeclareLaunchArgument(
+            "urdf_xacro_name",
+            description="The full file name of the xacro which creates the URDF.",
+        ),
+        DeclareLaunchArgument(
+            "x",
+            description="X coordinate of the entity.",
+            default_value='0.0'
+        ),
+        DeclareLaunchArgument(
+            "y",
+            description="Y coordinate of the entity.",
+            default_value='0.0'
+        ),
+        DeclareLaunchArgument(
+            "z",
+            description="Z coordinate of the entity.",
+            default_value='0.0'
+        ),
+        DeclareLaunchArgument(
+            "R",
+            description="The roll of the entity.",
+            default_value='0.0'
+        ),
+        DeclareLaunchArgument(
+            "P",
+            description="The pitch of the entity.",
+            default_value='0.0'
+        ),
+        DeclareLaunchArgument(
+            "Y",
+            description="The yaw of the entity.",
+            default_value='0.0'
+        ),
+        DeclareLaunchArgument(
+            "urdf_args",
+            description="Additional arguments for the URDF.",
+            default_value=None
+        )
+    ]
+
+    #print()
+    #print('ruc')
+    #for arg in declared_arguments:
+    #    print(arg)
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
---
+++ ros2_ws/src/robotcontrol/scripts/moveit_action_client.py
@@ -0,0 +1,142 @@
+import math
+import threading
+import time
+from threading import Event
+
+import numpy as np
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from rcontrol_msgs.action import MoveCartesian, MoveJoint, MovePose
+
+
+class MoveItActionClient(Node):
+    def __init__(self):
+        super().__init__('moveit_action_client')
+        self.action_done_event = Event()
+        self.result = False
+
+        self._movecartesian_client = ActionClient(self, MoveCartesian, 'movecartesian')
+        self._movejoint_client = ActionClient(self, MoveJoint, 'movejoint')
+        self._movepose_client = ActionClient(self, MovePose, 'movepose')
+
+
+    def move_cartesian(self, pos):
+        self.action_done_event.clear()
+        self.result = False
+
+        goal_msg = MoveCartesian.Goal()
+        goal_msg.posevalues = pos
+
+        self._movecartesian_client.wait_for_server()
+
+        self._send_goal_future = self._movecartesian_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+        self.action_done_event.wait()
+
+    def move_joint(self, pos):
+        self.action_done_event.clear()
+        self.result = False
+
+        goal_msg = MoveJoint.Goal()
+        goal_msg.jointvalues = pos
+
+        self._movejoint_client.wait_for_server()
+
+        self._send_goal_future = self._movejoint_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+        self.action_done_event.wait()
+
+    def move_pose(self, pos):
+        self.action_done_event.clear()
+        self.result = False
+
+        goal_msg = MovePose.Goal()
+        goal_msg.posevalues = pos
+
+        self._movepose_client.wait_for_server()
+
+        self._send_goal_future = self._movepose_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+        self.action_done_event.wait()
+
+
+    
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.result = result.success
+        self.get_logger().info('Result: {0}'.format(result.success))
+        self.action_done_event.set()
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback: {0}'.format(feedback.status))
+
+
+def run_executor(executor, n):
+    executor.add_node(n)
+    try:
+        while True:
+            executor.spin_once()
+            time.sleep(0.01)
+    finally:
+        executor.shutdown()
+
+def quaternion_from_euler(roll, pitch, yaw):
+    """
+    Converts euler roll, pitch, yaw to quaternion (w in last place)
+    quat = [x, y, z, w]
+    Bellow should be replaced when porting for ROS 2 Python tf_conversions is done.
+    """
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+
+    q = [0] * 4
+    q[0] = cy * cp * cr + sy * sp * sr
+    q[1] = cy * cp * sr - sy * sp * cr
+    q[2] = sy * cp * sr + cy * sp * cr
+    q[3] = sy * cp * cr - cy * sp * sr
+
+    return q
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = MoveItActionClient()
+
+    exec = MultiThreadedExecutor(num_threads=4)
+    thread = threading.Thread(target=run_executor, args=(exec, node), daemon=True)
+    thread.start()
+
+    pose = [0.2, 0.2, 0.5, 0.0, 0.0, 0.0, 0.0]
+
+    q = quaternion_from_euler(np.pi/2, 0.0, 0.0)
+    pose[3] = q[0]
+    pose[4] = q[1]
+    pose[5] = q[2]
+    pose[6] = q[3]
+
+    node.move_pose(pose)
+
+if __name__ == '__main__':
+    main()
