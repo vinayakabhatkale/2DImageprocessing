8cb2fc6a8af0c5700d3644001d706ec37fea70e1
Max Schnitzler <maximilian.schnitzler@hs-augsburg.de>
2022-12-01 16:39:34 +0100

    Cleanup

--- ros2_ws/src/build/octomap_msgs/CMakeFiles/octomap_msgs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/octomap_msgs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/octomap_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros2_ws/src/build/octomap_msgs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ModProFT/src/octomap_msgs
-# Build directory: /root/ModProFT/src/build/octomap_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("octomap_msgs__py")
